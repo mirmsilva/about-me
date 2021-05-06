83ca4d7865a04280fe2511bf15e407f29d47c469
Miriam Silva <miriam-silva@live.com>
2021-05-06 08:50:49 -0700

    edited question & answer prompt wording

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
-# LAB - 02
+# LAB - 03
 
 ## About Me Guessing Game
 
 An about me page with five yes or no questions so that my peers may get to know me better.
+
+Lab 03 Changes:
+- added a number guessing game
+- added an array guessing game
+- added a top ten ordered list in HTML
+- added a counter that would add up user's correct answers
+- changed my work experience into a list
 
 ### Author: Miriam Silva
 
--- js/app.js
+++ js/app.js
@@ -7,42 +7,35 @@ let userName = prompt ('Hello! What is your name?');
 console.log(userName);
 alert('Welcome '+ userName);
 
-// My location
+// Question 1: Location
 let userLocation = prompt ('Do I live in Seattle? yes/no?');
 console.log(userLocation);
 
-// Make room for upper/lower case
 let lowerCaseLocation = userLocation.toLowerCase();
-let upperCaseLocation = userLocation.toUpperCase();
-
 console.log(lowerCaseLocation);
-console.log(upperCaseLocation);
 
 let rightAnswerCount = 0;
 
-// Do I live in Seattle response alerts. Yes/No
+//Question 1 Answers
 if (lowerCaseLocation === 'yes'|| lowerCaseLocation ==='y'){
   alert('You are right!');
   //console.log ('right');
 }else if (lowerCaseLocation ==='no' || lowerCaseLocation ==='n'){
-  alert('That is not the answer. Try again!');
+  alert('Actually, I do live in Seattle');
   //console.log ('wrong');
 }else{
   alert('Hmm I did not understand your answer. Please respond with yes or no');
   //console.log ('try again');
 }
 
-// Am I a student prompt. Yes/No
+// Question 2: Student
 let userStudent = prompt ('Am I a Code Fellows Student? yes/no');
 console.log(userStudent);
 
 let lowerCaseStudent = userStudent.toLowerCase();
-let upperCaseStudent = userStudent.toUpperCase();
-
 console.log(lowerCaseStudent);
-console.log(upperCaseStudent);
 
-// Am I a student response alerts. Yes/No
+// Question 2 Answers
 if (lowerCaseStudent ==='yes' || lowerCaseStudent=='y'){
   alert ('That is correct');
   rightAnswerCount ++;
@@ -55,17 +48,14 @@ if (lowerCaseStudent ==='yes' || lowerCaseStudent=='y'){
   //console.log ('try again');
 }
 
-// Do I enjoy ice cream? Yes/No
+// Question 3: Ice Cream
 let userIceCream = prompt('Do I enjoy Ice Cream? yes/no');
 console.log(userIceCream);
 
 let lowerCaseIceCream = userIceCream.toLowerCase();
-let upperCaseIceCream = userIceCream.toUpperCase();
-
 console.log(lowerCaseIceCream);
-console.log(upperCaseIceCream);
 
-// Do I like Ice Cream response alerts. Yes/No
+// Question 3 Answers
 if(lowerCaseIceCream === 'yes' || lowerCaseIceCream ==='y'){
   alert('That is correct! My favorite flavor is Rocky Road!');
   rightAnswerCount ++;
@@ -78,55 +68,48 @@ if(lowerCaseIceCream === 'yes' || lowerCaseIceCream ==='y'){
   //console.log ('try again');
 }
 
-alert ('you got this many correct Answers' + rightAnswerCount);
-
-// Am I cat lady? Yes/No
+// Question 4: Cats
 let userCatLady = prompt('Am I a Cat Lady? yes/no');
 console.log(userCatLady);
 
 let lowerCaseCat = userCatLady.toLowerCase();
-let upperCaseCat = userCatLady.toUpperCase();
-
 console.log(lowerCaseCat);
-console.log(upperCaseCat);
 
-// Cat Lady response alerts. Yes/No
+// Question 4 Answers
 if (lowerCaseCat ==='yes' || lowerCaseCat ==='y'){
   alert ('Alot of people would agree with you!');
+  rightAnswerCount ++;
   //console.log ('right');
 }else if(lowerCaseCat ==='no' || lowerCaseCat ==='n'){
-  alert('Try again! I am deeply obsessed with my cats');
+  alert('I am deeply obsessed with my cats');
   //console.log ('wrong');
 }else{
   alert('Hmm I did not understand your answer. Please respond with yes or no');
   //console.log ('try again');
 }
 
-// Am I a huge Harry Potter nerd. Yes/no
+// Question 5: Harry Potter
 let userHarryPotter = prompt ('Am I huge Harry Potter Nerd?, yes/no')
 console.log(userHarryPotter);
 
 let lowerCaseHarry = userHarryPotter.toLowerCase();
-let upperCaseHarry = userHarryPotter.toUpperCase();
-
 console.log(lowerCaseHarry);
-console.log(upperCaseHarry);
 
-// Harry Potter response alerts. Yes/No
+// Question 5 Answers
 if (lowerCaseHarry === 'yes' || lowerCaseHarry ==='y'){
   alert(userName + ', you are correct!')
+  rightAnswerCount ++;
   //console.log ('right');
 }else if (lowerCaseHarry === 'no' || lowerCaseHarry === 'n'){
-  alert (userName + ' , you are looking at a Hufflepuff. Try again!');
+  alert (userName + ' , you are looking at a Hufflepuff!');
   //console.log ('wrong');
 }else{
   alert('Hmm I did not understand your answer. Please respond with yes or no');
 }
 
+alert ('you got this many correct Answers' + rightAnswerCount);
 
-
-
-//Number Guessing Game     
+// Number Guessing Game     
 let correctNum = 3;
 
 for (let i = 0; i<5; i++){
@@ -143,13 +126,13 @@ for (let i = 0; i<5; i++){
 }
 
 
-//Array Guessing Game
+// Array Guessing Game
 let flavorArray = ['strawberry', 'chocolate', 'vanilla', 'rocky road','cookie dough'];
 let counter = 5;
 let rightAnswer = false;
 while(rightAnswer === false && counter >0){
   alert ('you have ' + counter + ' guesses left');
-  let flavorGuess1 = prompt ('What is my favorite ice cream flavor?');
+  let flavorGuess1 = prompt ('What is one of my favorite ice cream flavors?');
   let flavorGuess = flavorGuess1.toLowerCase();
   for(let i =0; i < flavorArray.length; i++){
     if(flavorGuess === flavorArray[1]){
